a9baec70a7384723429459fae7e9375b68d35ca9
André F. Martins <martins.adw@gmail.com>
2022-03-02 09:36:40 -0300

    Refactor reader functions into a separate file

---
+++ reader.py
@@ -0,0 +1,86 @@
+import math
+
+from weapon_data import weapon_type
+from artifact_data import percent_stats
+
+
+def read_artifacts(good_data):
+    artifact_data = {
+        'flower': [],
+        'plume': [],
+        'sands': [],
+        'goblet': [],
+        'circlet': []
+    }
+
+    for artifact in good_data['artifacts']:
+        new_artifact = {
+            'id': artifact['Id'],
+            'level': artifact['level'],
+            'set_key': artifact['setKey'],
+            'main_stat_key': artifact['mainStatKey'],
+            'sub_stats': {},
+            'empty_sub_stats': 0,
+            'missing_sub_stats': math.ceil((20 - artifact['level']) / 4)
+        }
+        for sub_stat in artifact['substats']:
+            if sub_stat['key'] is None:
+                new_artifact['empty_sub_stats'] += 1
+            elif sub_stat['key'] in percent_stats:
+                new_artifact['sub_stats'][sub_stat['key']] = sub_stat['value'] / 100
+            else:
+                new_artifact['sub_stats'][sub_stat['key']] = sub_stat['value']
+
+        artifact_data[artifact['slotKey']].append(new_artifact)
+
+    return artifact_data
+
+
+def read_weapons(good_data):
+    weapon_data = {
+        'bow': [],
+        'catalyst': [],
+        'polearm': [],
+        'claymore': [],
+        'sword': []
+    }
+
+    for weapon_id, weapon in enumerate(good_data['weapons']):
+        ascension = weapon['ascension']
+        max_level = (20 + ascension * 20) if ascension <= 1 else (40 + (ascension - 1) * 10)
+        new_weapon = {
+            'id': weapon_id,
+            'level': weapon['level'],
+            'max_level': weapon['level'],
+            'ascension': ascension,
+            'refinement': weapon['refinement'],
+            'key': weapon['key'],
+            'type': weapon_type[weapon['key']]
+        }
+
+        weapon_data[new_weapon['type']].append(new_weapon)
+
+    return weapon_data
+
+
+def read_characters(good_data):
+    characters_data = []
+
+    for character_id, character in enumerate(good_data['characters']):
+        ascension = character['ascension']
+        max_level = (20 + ascension * 20) if ascension <= 1 else (40 + (ascension - 1) * 10)
+        new_character = {
+            'id': character_id,
+            'level': character['level'],
+            'max_level': max_level,
+            'ascension': ascension,
+            'constellation': character['constellation'],
+            'talent_1': character['talent']['auto'],
+            'talent_2': character['talent']['skill'],
+            'talent_3': character['talent']['burst'],
+            'key': character['key']
+        }
+
+        characters_data.append(new_character)
+
+    return characters_data
